af3aae51c503f64d7d421587f683691916bb1a85
MaureenGichuki <maureen.gichuki@student.moringaschool.com>
2022-04-20 11:24:06 +0300

    "Imported supporting modules and components"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { RepoComponent } from './repo/repo.component';
+import { UserComponent } from './user/user.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './navbar/navbar.component';
+import { WaruguruRoutingModule } from './waruguru/waruguru-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RepoComponent,
+    UserComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    WaruguruRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
